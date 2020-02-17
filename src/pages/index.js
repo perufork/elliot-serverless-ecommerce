1b93e3a1d277297f81745c804d9324b82d437b9c
smakosh <ismai23l@hotmail.com>
2020-02-18 00:48:47 +0100

    chore: experimenting with locales

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import { useEffect } from "react";
 // import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 // import { getBrowserLocale } from "helpers/locale";
-import { useIntl } from "react-intl";
 
 const Index = () => {
 	// const Router = useRouter();
-	const { locale } = useIntl();
 
 	useEffect(() => {
-		Router.replace("/[lang]", `/${locale}/`);
+		Router.replace("/[lang]", `/en/`);
 	}, []);
 
 	return (
